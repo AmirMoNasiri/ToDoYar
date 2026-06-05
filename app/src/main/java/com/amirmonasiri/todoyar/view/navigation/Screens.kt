82b319ce04e48c6d7aa1efb59eb4cbdcbfd8332f
amirmonasiri <amirmonasiri@gmail.com>
2026-06-05 20:23:14 +0330

    add settings objects and update Icons

--- app/src/main/java/com/amirmonasiri/todoyar/view/navigation/Screens.kt
+++ app/src/main/java/com/amirmonasiri/todoyar/view/navigation/Screens.kt
@@ -1,12 +1,14 @@
 package com.amirmonasiri.todoyar.view.navigation
 
 import androidx.annotation.StringRes
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Autorenew
-import androidx.compose.material.icons.rounded.CalendarMonth
-import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material.icons.rounded.Task
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.adamglin.PhosphorIcons
+import com.adamglin.phosphoricons.Duotone
+import com.adamglin.phosphoricons.duotone.AndroidLogo
+import com.adamglin.phosphoricons.duotone.CalendarDots
+import com.adamglin.phosphoricons.duotone.CheckSquare
+import com.adamglin.phosphoricons.duotone.Gear
+import com.adamglin.phosphoricons.duotone.User
 import com.amirmonasiri.todoyar.R
 
 sealed class Screens(
@@ -14,10 +16,11 @@ sealed class Screens(
     @StringRes val titleRes: Int,
     val icon: ImageVector
 ) {
-    object Splash: Screens("splash", R.string.splash, Icons.Rounded.Autorenew)
-    object Tasks : Screens("tasks", R.string.nav_tasks, Icons.Rounded.Task)
-    object Calendar : Screens("calendar", R.string.nav_calendar, Icons.Rounded.CalendarMonth)
-    object Profile : Screens("profile", R.string.nav_profile, Icons.Rounded.Person)
+    object Splash : Screens("splash", R.string.splash, PhosphorIcons.Duotone.AndroidLogo)
+    object Tasks : Screens("tasks", R.string.nav_tasks, PhosphorIcons.Duotone.CheckSquare)
+    object Calendar : Screens("calendar", R.string.nav_calendar, PhosphorIcons.Duotone.CalendarDots)
+    object Profile : Screens("profile", R.string.nav_profile, PhosphorIcons.Duotone.User)
+    object Settings : Screens("settings", R.string.settings, PhosphorIcons.Duotone.Gear)
 
     companion object {
         val MainScreens = listOf(Tasks, Calendar, Profile)
