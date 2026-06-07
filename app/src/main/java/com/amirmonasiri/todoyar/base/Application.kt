e33f8a14b02de92a9e79074e4c686b13e227bf9e
amirmonasiri <amirmonasiri@gmail.com>
2026-06-07 20:50:57 +0330

    check dagger hilt in project

--- app/src/main/java/com/amirmonasiri/todoyar/base/Application.kt
+++ app/src/main/java/com/amirmonasiri/todoyar/base/Application.kt
@@ -3,6 +3,5 @@ package com.amirmonasiri.todoyar.base
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-
 @HiltAndroidApp
-class Application: Application()
+class Application : Application()
