594c27219e70ae05f3f1ecead03300d577c90dd4
amirmonasiri <amirmonasiri@gmail.com>
2026-06-09 22:18:59 +0330

    clean MainActivity and add toDoYarNavHost composable

--- app/src/main/java/com/amirmonasiri/todoyar/MainActivity.kt
+++ app/src/main/java/com/amirmonasiri/todoyar/MainActivity.kt
@@ -4,32 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.amirmonasiri.todoyar.view.navigation.navigationBarScreen
-import com.amirmonasiri.todoyar.view.navigation.setupNavigation
+import com.amirmonasiri.todoyar.view.navigation.toDoYarNavHost
 import com.amirmonasiri.todoyar.view.ui.theme.ToDoYarTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ToDoYarTheme {
-                val navController = rememberNavController()
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    bottomBar = {
-                        navigationBarScreen(navController)
-                    }
-                ) { innerPadding ->
-                    setupNavigation(navController, innerPadding)
-                }
+                toDoYarNavHost()
             }
         }
     }
