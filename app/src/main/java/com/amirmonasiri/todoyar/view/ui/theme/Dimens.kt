72e794fc694f77920990f50423aad24414ebbf4c
amirmonasiri <amirmonasiri@gmail.com>
2026-06-10 04:04:02 +0330

    Add Dimens file to theme

---
+++ app/src/main/java/com/amirmonasiri/todoyar/view/ui/theme/Dimens.kt
@@ -0,0 +1,34 @@
+package com.amirmonasiri.todoyar.view.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    // (Spacings / Paddings / Margins)
+    val space4 = 4.dp
+    val space8 = 8.dp
+    val space12 = 12.dp
+    val space16 = 16.dp
+    val space20 = 20.dp
+    val space24 = 24.dp
+    val space32 = 32.dp
+
+    // Icons
+    val iconSmall = 16.dp
+    val iconDefault = 24.dp
+    val iconMedium = 32.dp
+    val iconLarge = 48.dp
+    val iconBottomBar = 36.dp
+
+    // Components Height
+    val bottomBarHeight = 80.dp
+    val topBarHeight = 56.dp
+    val buttonHeight = 48.dp
+
+    // Corner Radius
+    val cornerSmall = 4.dp
+    val cornerMedium = 8.dp
+    val cornerLarge = 12.dp
+    val cornerExtraLarge = 16.dp
+    val cornerBottomSheet = 20.dp
+}
