2e7172f3bfd0d614637430853ccf010616c246d2
amirmonasiri <amirmonasiri@gmail.com>
2026-06-13 16:21:52 +0330

    use flow in viewmodel for change theme

--- app/src/main/java/com/amirmonasiri/todoyar/viewModel/SettingsViewModel.kt
+++ app/src/main/java/com/amirmonasiri/todoyar/viewModel/SettingsViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.viewModelScope
 import com.amirmonasiri.todoyar.data.repository.SettingsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,25 +17,16 @@ class SettingsViewModel @Inject constructor(
     private val settingsRepository: SettingsRepository
 ) : ViewModel() {
 
-    private val _isDarkTheme = MutableStateFlow(false)
-    val isDarkTheme: StateFlow<Boolean> = _isDarkTheme.asStateFlow()
-
-    init {
-        loadThemePreference()
-    }
-
-    private fun loadThemePreference() {
-        viewModelScope.launch {
-            settingsRepository.darkTheme.collect { isDark ->
-                _isDarkTheme.value = isDark
-            }
-        }
-    }
+    val isDarkTheme = settingsRepository.darkTheme
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = false
+        )
 
     fun setDarkTheme(enabled: Boolean) {
         viewModelScope.launch {
             settingsRepository.setDarkTheme(enabled)
         }
     }
-
 }
