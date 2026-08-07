4cc57e85a0f879e132260c09eaf2f6622d378ae2
amirmonasiri <amirmonasiri@gmail.com>
2026-08-07 04:00:19 +0330

    add splash screen

--- app/src/main/java/com/amirmonasiri/todoyar/view/screens/SplashScreen.kt
+++ app/src/main/java/com/amirmonasiri/todoyar/view/screens/SplashScreen.kt
@@ -1,25 +1,67 @@
 package com.amirmonasiri.todoyar.view.screens
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.amirmonasiri.todoyar.R
+import com.amirmonasiri.todoyar.navigation.Screens
+import com.amirmonasiri.todoyar.view.ui.theme.PersianBlue
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    LaunchedEffect(Unit) {
+        delay(2000)
+        navController.navigate(Screens.Main.route) {
+            popUpTo(Screens.Splash.route) {
+                inclusive = true
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(PersianBlue),
     ) {
-        Text(
-            "Splash Screen",
-            fontSize = 28.sp
+        Image(
+            painter = painterResource(R.drawable.ic_todoyar_noback),
+            contentDescription = "To Do Yar Icon",
+            modifier = Modifier
+                .size(160.dp)
+                .align(Alignment.Center)
         )
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(32.dp), color = Color.White, strokeWidth = 3.dp
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = "نسخه 1.0.0",
+                color = Color.White,
+            )
+        }
     }
 }
